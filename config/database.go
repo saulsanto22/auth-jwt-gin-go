44d6fbadf3782a4ad55cac6611417c4693644798
saul <saul.santoa@gmail.com>
2025-11-05 13:45:48 +0700

    rafactor code

--- config/database.go
+++ config/database.go
@@ -1,8 +1,11 @@
 package config
 
 import (
+	"fmt"
 	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -10,11 +13,26 @@ import (
 var DB *gorm.DB
 
 func ConnectDB() {
-	dsn := "root:@tcp(127.0.0.1:3306)/go_rest?charset=utf8mb4&parseTime=True&loc=Local"
+	// dsn := "root:@tcp(127.0.0.1:3306)/go_rest?charset=utf8mb4&parseTime=True&loc=Local"
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("⚠️  Tidak menemukan file .env, menggunakan environment default...")
+	}
+
+	user := os.Getenv("DB_USER")
+	pass := os.Getenv("DB_PASS")
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	name := os.Getenv("DB_NAME")
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		user, pass, host, port, name)
+
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
-		log.Fatal("Koneksi gagal!")
+		log.Println("⚠️  Tidak menemukan file .env, menggunakan environment default...")
 	}
 
 	DB = db
